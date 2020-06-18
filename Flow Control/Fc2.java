3adc3a9fd956f637410bd8c05c9ff813fa4d0dad
grootshub <60656592+grootshub@users.noreply.github.com>
2020-06-18 13:26:08 +0530

    Add files via upload

---
+++ Flow Control/Fc2.java
@@ -0,0 +1,14 @@
+public class Fc2{
+public static void main(String args[])
+	{
+	    int number1=Integer.parseInt(args[0]);
+	    if(number1%2==0)
+		{
+		  System.out.println("Even");
+		}
+	    else
+		{
+		  System.out.println("Odd");
+		}
+	}
+}
---
+++ Flow Control/Fc4.java
@@ -0,0 +1,15 @@
+public class Fc4{
+public static void main(String args[]){
+	char sample1='s';
+	char sample2='e';
+	if(sample2<sample1)
+	  {
+          System.out.println(sample2+","+sample1);
+	  }
+	else
+	  {
+	  System.out.println(sample1+","+sample2);
+	  }
+   }
+}
+	
